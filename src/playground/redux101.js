e135a2b66f9d08d89156785fbe65b9744f828a83
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2018-01-23 23:28:36 +0800

    redux

--- src/playground/redux101.js
+++ src/playground/redux101.js
@@ -1,8 +1,36 @@
 import { createStore } from 'redux';
 
 // set default state
-const store = createStore((state = { count : 0}) => {
-    return state;
+const store = createStore((state = { count : 0}, action) => {
+    switch (action.type) {
+        case 'INCREMENT': 
+            return { count: state.count + 1 };
+        case 'DECREMENT': 
+            return {count: state.count - 1};
+        case 'RESET': 
+            return {count: 0}
+        default:
+            return state;
+    }
 });
 
 console.log(store.getState());
+// actions - change redux store state - obj that gets sent to redux store 
+
+// increment
+store.dispatch({
+    type: 'INCREMENT'
+});
+console.log(store.getState());
+
+store.dispatch({type: 'DECREMENT'});
+store.dispatch({type: 'DECREMENT'});
+console.log(store.getState());
+
+
+store.dispatch({type: 'RESET'});
+console.log(store.getState());
+
+// reset
+
+
