bb96146c5121418e16677ea379e89f3e35dd2936
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2018-01-26 22:46:12 +0800

    test multiple redux reducers for complex objects

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,6 +1,37 @@
 import { createStore, combineReducers } from 'redux';
 
 // expense state, add, remove, edit, filters
+// sort by date
+// --> break up application in many reducers
+// 1. Expenses Reducer 2. Filters Reducer
+
+const expensesDefaultState = [];
+
+const expensesReducer = (state = expensesDefaultState, action) => {
+ switch(action.type) {
+     default:
+        return state;
+ }
+}
+
+const filterDefaultState = {text: '', sortBy: 'date', startDate: undefined, endDate: undefined};
+
+const filterReducer = (state = filterDefaultState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+};
+// create store
+const store = createStore(
+    combineReducers({
+        // set up expensesReducer for expenses
+        expenses: expensesReducer,
+        filter: filterReducer
+    })
+);
+
+console.log(store.getState());
 const demoState = {
     expenses: [{
         id: 'posjpei',
