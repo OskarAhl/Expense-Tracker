a40719bfc36c8425c9001547863be4d7fb0c7009
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2018-01-30 23:38:22 +0800

    add test cases expenses actions

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -10,3 +10,48 @@ test('should setup remove expense action object', () => {
         id: '123abc'
     });
 });
+
+test('Should setup edit expense action object', () => {
+    const action = editExpense('123abc', { note: 'new note value' });
+    expect(action).toEqual({
+        type: 'EDIT_EXPENSE',
+        id: '123abc',
+        updates: {
+            note: 'new note value'
+        }
+    });
+});
+
+test('Should setup add expense action object with provided values', () => {
+    const expenseData = {
+        description: 'rent',
+        amount: 1095,
+        createdAt: 1000,
+        note: 'This was last months rent'
+    }
+    const action = addExpense(expenseData);
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            ...expenseData,
+            id: expect.any(String)
+        }
+    });
+});
+
+test('Should setup add expense action object with no provided values', () => {
+    const expenseData = {
+        description: '', 
+        note:'', 
+        amount: 0, 
+        createdAt: 0
+    };
+    const action = addExpense();
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            ...expenseData,
+            id: expect.any(String)
+        }
+    });
+});
