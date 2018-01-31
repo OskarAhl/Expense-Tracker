79707522bc80228c642ad502a63af9b1c353af6c
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2018-01-31 16:24:50 +0800

    add snapshot tests for ExpenseDashboard, ExpenseList, ExpenseListItem, NotFound components

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -4,22 +4,26 @@ import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from '../selectors/expenses';
 
 // when state changes component will be re-rendered with the new state
-const ExpenseList = (props) => (
+// export unconnected (from state) component for testing
+export const ExpenseList = (props) => (
     <div>
-        <h1>ExpenseList</h1>
         {
-            props.expenses.map((expense) => (
-                <ExpenseListItem 
-                    key={expense.id} 
-                    {...expense}
-                />
-            ))
+            props.expenses.length === 0 ? (
+                <p>No expenses</p>
+            )   : (
+                props.expenses.map((expense) => (
+                    <ExpenseListItem 
+                        key={expense.id} 
+                        {...expense}
+                    />
+                ))
+            )
         }
     </div>
 );
 
 const mapStateToProps = (state) => {
-    // what information from store component should be allowed to access
+    // what information from store - component should be allowed to access
     return {
         expenses: selectExpenses(state.expenses, state.filters),
     };
