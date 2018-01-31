0b9117d6b4871eedd04a8aebe15d236a5bba42bd
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2018-01-31 12:21:58 +0800

    add react-test-renderer and snapshot test for Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,11 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Header = () => (
-    <div>
-        <header>
-            <h1>Expensify</h1>
-            <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
-            <NavLink to="/create" activeClassName="is-active"> Create</NavLink>
-            <NavLink to="/help" activeClassName="is-active">Help</NavLink>
-        </header>
-    </div>
+    <header>
+        <h1>Expensify</h1>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
+        <NavLink to="/create" activeClassName="is-active"> Create</NavLink>
+    </header>
 );
 
 export default Header;
