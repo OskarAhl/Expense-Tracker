72dc0f5cab24515ca452eb7175398b9388d9136e
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2018-02-01 11:38:32 +0800

    remove seed data

--- src/app.js
+++ src/app.js
@@ -14,14 +14,6 @@ import 'react-dates/lib/css/_datepicker.css';
 // gives access to store.dispatch, store.subcscribe etc
 const store = configureStore();
 
-store.dispatch(addExpense({description : 'Water', amount: 10, createdAt: 5}));
-store.dispatch(addExpense({description : 'Rent', amount: 1800, createdAt: 10}));
-store.dispatch(addExpense({description : 'Mobile', amount: 60, createdAt: 15}));
-store.dispatch(addExpense({description : 'Gas', amount: 5, createdAt: 20}));
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters); 
-console.log(visibleExpenses);
-
 const jsx = (
     // all components can have access to the store
     <Provider store={store}> 
