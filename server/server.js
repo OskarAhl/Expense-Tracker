c47cf1e7f3e18685f3bf7a63ed2994e09a18bfdd
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2018-02-01 13:15:01 +0800

    setup production build and server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
 
 // serve public folder and everything inside
 // register middleware (runs with each express)
@@ -12,6 +13,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('server is up!');
 })
