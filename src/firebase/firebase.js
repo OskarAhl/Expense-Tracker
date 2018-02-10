7ab5891f81862091bfe2cb3af08796a078f8334c
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2018-02-10 10:36:27 +0800

    firebase initial setup

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// * take ALL named exports and put them under firebase
+import * as firebase from 'firebase';
+
+// make connection to db
+const config = {
+    apiKey: "AIzaSyAx3HKsGx4MVEsnmfxV2GadjLJ0xIxXE18",
+    authDomain: "expense-tracker-8ffda.firebaseapp.com",
+    databaseURL: "https://expense-tracker-8ffda.firebaseio.com",
+    projectId: "expense-tracker-8ffda",
+    storageBucket: "expense-tracker-8ffda.appspot.com",
+    messagingSenderId: "358471724244"
+  };
+  firebase.initializeApp(config);
+
+  firebase.database().ref().set({
+    name: 'Oskar'
+  });
