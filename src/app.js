e4fd2d4bb28995c45ac81350c23e969f14fda272
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2018-02-26 23:47:42 +0800

    setup getting data from firebase

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
+import { startSetExpenses } from './actions/expenses';
 import { setTextFilter } from './actions/filters'
 import getVisibleExpenses from './selectors/expenses';
 // normalize => reset browser default css
@@ -21,4 +21,9 @@ const jsx = (
     </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then((data) => {
+    ReactDOM.render(jsx, document.getElementById('app'));
+});
+
