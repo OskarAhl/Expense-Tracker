166be285e8a0c29aaa7bbf4a1b8d364f2f3ebae9
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2018-03-04 16:08:14 +0800

    change db structure for expenses for separate users

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -8,7 +8,8 @@ export const addExpense = (expense) => ({
 });
 
 export const startAddExpense = (expenseData = {}) => {
-    return (dispatch) => {
+    return (dispatch, getState) => {
+        const uid = getState().auth.uid;
         // dispatch after expense added in fb
         const {
             description = '', 
@@ -18,7 +19,7 @@ export const startAddExpense = (expenseData = {}) => {
         } = expenseData;
         const expense = { description, note, amount, createdAt };
         
-        return database.ref('expenses').push(expense).then((ref) => {
+        return database.ref(`users/${uid}/expenses`).push(expense).then((ref) => {
             dispatch(addExpense({
                 id: ref.key,
                 ...expense
@@ -64,8 +65,9 @@ export const setExpenses = (expenses) => ({
 
 // export const startSetExpenses
 export const startSetExpenses = () => {
-    return (dispatch) => {
-        return database.ref('expenses').once('value').then((snapshot) => {
+    return (dispatch, getState) => {
+        const uid = getState().auth.uid;
+        return database.ref(`users/${uid}/expenses`).once('value').then((snapshot) => {
             const expenses = [];
             snapshot.forEach((childSnapshot) => {
                 expenses.push({
