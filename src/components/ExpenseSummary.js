bfb5554772b9243729cb581382bd1ee806e76b68
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2018-03-04 20:23:36 +0800

    add expenses summary style

--- src/components/ExpenseSummary.js
+++ src/components/ExpenseSummary.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import selectExpenses from '../selectors/expenses';
 import { getExpensesTotal, getCountTotal } from '../selectors/expenses-total';
 import numeral from 'numeral';
 
-export const ExpenseSummary = (props) => (
-    <div> 
-        <h4>
-            Viewing { getCountTotal(props.expenses) + ' ' } 
-            totalling { numeral(getExpensesTotal(props.expenses)).format('$0,0.00') } 
-        </h4>
-    </div>
-);
+export const ExpenseSummary = (props) => {
+    const expenseWord = props.expenses.length === 1 ? 'expense' : 'expenses';
+    return (
+        <div className="page-header"> 
+            <div className="content-container">
+                <h1 className="page-header__title">
+                    Viewing <span> { props.expenses.length } </span> {  expenseWord + ' ' }
+                    totalling <span> { numeral(getExpensesTotal(props.expenses)).format('$0,0.00') } </span>
+                </h1>
+                <div className="page-header__actions">
+                    <Link className="button" to="/create"> Add Expense </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 const mapStateToProps = (state) => ({
         expenses: selectExpenses(state.expenses, state.filters),
