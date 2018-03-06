65f729d700a230e86e37c8c1d7eff4fa6ceef307
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2018-03-06 23:17:37 +0800

    add loader gif

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ import 'react-dates/lib/css/_datepicker.css';
 import './firebase/firebase';
 
 import {firebase} from './firebase/firebase';
+import LoadingPage from './components/LoadingPage';
 
 // gives access to store.dispatch, store.subcscribe etc
 const store = configureStore();
@@ -31,7 +32,7 @@ const renderApp = () => {
     }
 }
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
 // Auth state changed --> user login, logout callback
 firebase.auth().onAuthStateChanged((user) => {
